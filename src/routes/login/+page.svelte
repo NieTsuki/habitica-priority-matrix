a1ac7ff9ed5b265d9a699ff6baf68300cc4a6d31
Nie Tsuki <nie.0x6708@gmail.com>
2025-08-09 15:36:56 +0800

    Add cursor-pointer class

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -70,7 +70,7 @@
           <button
             type="submit"
             disabled={isLoading}
-            class="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#6133b4] hover:bg-[#4f2a93] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#6133b4] disabled:opacity-50 disabled:cursor-not-allowed transition-colors"
+            class="w-full flex justify-center py-3 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-[#6133b4] hover:bg-[#4f2a93] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#6133b4] disabled:opacity-50 disabled:cursor-not-allowed transition-colors cursor-pointer"
           >
             {#if isLoading}
               <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
