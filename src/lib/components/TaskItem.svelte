d25a8971f4609583a775d48faa77aa6d27ab7898
Nie Tsuki <nie.0x6708@gmail.com>
2025-08-11 14:08:06 +0800

    Display task notes

--- src/lib/components/TaskItem.svelte
+++ src/lib/components/TaskItem.svelte
@@ -5,14 +5,20 @@
     const { task } = $props<{ task: Task }>();
 </script>
 
-<div class="flex bg-white rounded-lg shadow-sm border border-gray-200 overflow-hidden p-4 items-center justify-between">
-    <div class="flex items-center">
-        <div class="space-y-2">
+<div class="flex bg-white rounded-lg shadow-sm border border-gray-200 overflow-hidden p-4 items-center justify-between gap-2">
+    <div class="flex items-center grow">
+        <div class="space-y-2 w-full">
             <h3 class="font-medium text-gray-800 text-sm {task.completed ? 'line-through text-gray-500' : ''}">{task.text}</h3>
+
+            {#if task.notes}
+                <p class="text-xs text-gray-600 break-all">
+                    {task.notes.length > 128 ? task.notes.slice(0, 128) + '...' : task.notes}
+                </p>
+            {/if}
         </div>
     </div>
 
-    <div class="text-sm text-[#878190]">
+    <div class="text-sm text-[#878190] whitespace-nowrap">
         {#if task.date}
             {formatDueDate(task.date)}
         {/if}
